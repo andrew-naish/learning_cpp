c0ee2239311e84832a92763bd520733e715fa5b7
Andrew Naish <andy.naish1@gmail.com>
2017-07-15 01:35:52 +0100

    added linux build compatability

---
+++ buildall.sh
@@ -0,0 +1,8 @@
+find . -type f -name "*.cpp" -print0 | sort -z | while IFS= read -r -d '' file; do
+    directory=`dirname "$file"`
+    filename=`basename "$file" | cut -d'.' -f1`
+
+    echo "On: ${directory##*/}/$filename"
+        g++ "$file" -o "$directory/$filename"
+    echo ""
+done
