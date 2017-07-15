46d215f0e0408a2350637ac9452e290616b06da0
Andrew Naish <andy.naish1@gmail.com>
2017-07-15 03:09:08 +0100

    build & run for Windows + Linux yipeee!

--- buildall.sh
+++ buildall.sh
@@ -3,6 +3,6 @@ find . -type f -name "*.cpp" -print0 | sort -z | while IFS= read -r -d '' file; 
     filename=`basename "$file" | cut -d'.' -f1`
 
     echo "On: ${directory##*/}/$filename"
-        g++ "$file" -o "$directory/$filename"
+        g++ -g "$file" -o "$directory/$filename"
     echo ""
 done
